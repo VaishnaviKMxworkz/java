5dfc98eb034a035ffaba3bce3408410ab246504d
Vaishnavi K M <vaishnavikm.xworkz@gmail.com>
2024-06-20 14:21:19 -0400

    d martt class

---
+++ Dmartt.java
@@ -0,0 +1,13 @@
+class SportsMania{
+	static String iplTeamNames[]={"Royal Challengers Banglore" , "Chennai Super Kings" , "Mumbai Indians" , "Rajasthan Royals" , "Delhi Capitals" , "Gujarat Titans" , "Lucknow Super Giants" , "Punjab Kings", "Kolkata Knight Riders","Sunrisers Hyderabad"};
+	static String top10Batsman[]={"Sachin Tendulkar","Brian Lara", "Ricky Ponting","Sir Isaac Vivian Alexander Richards","Virat Kohli", "Kumar Chokshanada Sangakkara","Sir Donald George Bradman","Walter Reginald Hammond","Sir John Berry Hobbs","Sunil Gavaskar"};
+	static String top10Bowlers[]={"Muttiah Muralitharan","Shane Warne","Glenn McGrath","Wasim Akram","Brett Lee","Shoaib Akhtar","Dale Steyn","Mitchell Starc","Jasprit Bumrah","Lasith Malinga"};
+	static String top10IccTeamNames[]={"Afghanistan","Australia","Bangladesh","England","India","Ireland","New Zealand","Pakistan","South Africa","SriLanka"};
+	public static void main(String[] cricket){ 
+	System.out.println("The IPL team names are:" +iplTeamNames.length    +"  size    "   +iplTeamNames[0]+ "\n" +iplTeamNames[1]+ "\n" +iplTeamNames[2]+ "\n" +iplTeamNames[3]+ "\n" +iplTeamNames[4]+ "\n"+iplTeamNames[5]+ "\n" +iplTeamNames[6]+ "\n" +iplTeamNames[7]+ "\n" +iplTeamNames[8]+ "\n" +iplTeamNames[4]+ "\n"  +iplTeamNames[9]);
+	System.out.println("The top ten batsmans are:" +top10Batsman.length    +"  size    " +top10Batsman[0]+ "\n" +top10Batsman[1]+ "\n" +top10Batsman[2]+ "\n"  +top10Batsman[3]+ "\n" +top10Batsman[4]+ "\n" +top10Batsman[5]+ "\n" +top10Batsman[6]+ "\n" +top10Batsman[7]+ "\n" +top10Batsman[8]+ "\n" +top10Batsman[9]);
+	System.out.println("The top ten bowlers are:" +top10Bowlers[0]+ "\n" +top10Bowlers[1]+ "\n" +top10Bowlers[2]+ "\n" +top10Bowlers[3]+ "\n" +top10Bowlers[4]+ "\n" +top10Bowlers[5]+ "\n" +top10Bowlers[6]+ "\n" +top10Bowlers[7]+ "\n" +top10Bowlers[8]+ "\n" +top10Bowlers[9]);
+	System.out.println("The  top ten icc teams are:" +top10IccTeamNames[0]+ "\n" +top10IccTeamNames[1]+ "\n" +top10IccTeamNames[2]+ "\n" +top10IccTeamNames[3]+ "\n" +top10IccTeamNames[4]+ "\n" +top10IccTeamNames[5]+ "\n" +top10IccTeamNames[6]+ "\n" +top10IccTeamNames[7]+ "\n" +top10IccTeamNames[8]+ "\n" +top10IccTeamNames[9]);
+	}
+}	
+	
